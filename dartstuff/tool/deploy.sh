5423e33014d4080b36b0b7847fd6c6845b8dc155
Samuel Jackson <samjxn@iastate.edu>
2016-02-10 22:39:02 -0600

    added deploy script

---
+++ dartstuff/tool/deploy.sh
@@ -0,0 +1,20 @@
+#/bin/bash!
+
+SERVER=proj-309-16.cs.iastate.edu
+WEB_DIR=/var/www/html/
+
+echo -n "Enter username for proj-309-16.cs.iastate.edu > "
+echo
+read USERNM
+echo building dart project
+pub build
+echo compressing build
+tar -vzcf ./.build.tar.gz build/web/*
+echo connecting to server...
+scp .build.tar.gz $USERNM@$SERVER:$WEB_DIR
+echo
+echo You\'re going to have to enter a password again.  Sorry about that.
+echo
+ssh $USERNM@$SERVER 'cd /var/www/html/; tar -xzf .build.tar.gz; rm .build.tar.gz; cp -r build/web/* .; rm -r build/*; rmdir build'
+echo cleanup
+rm .build.tar.gz
